d7a98e115183e2060502b1770d15e897e01d7037
MarisolAL <marisolamezcua@ciencias.unam.mx>
2018-06-12 10:51:41 -0500

    Se anima un pasto y se cambio GL_POINTS por GL_LINE_STRIP

--- pasto.c
+++ pasto.c
@@ -6,10 +6,19 @@
  */
 
 #include <GL/glut.h>
+#include <math.h>
+#include <stdio.h>
 
+int tiempoActual, tiempoAnterior;
+double delta;
+static int x_i = 100;
+static GLfloat b= 300;
+static GLfloat a = 100*1.5;
+static int lim_sup = 100*1.5;
 static float x_pos = 0;
 static float y_pos = 0;
-static float z_pos = 0;
+static float z_pos = -0.1;
+
 
 void init() {
 	glClearColor(1.0, 1.0, 1.0, 0.0);
@@ -17,11 +26,79 @@ void init() {
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	gluOrtho2D(0, 480, 0, 640);
+	//glClear(GL_COLOR_BUFFER_BIT);
+}
+
+void calculaTiempo(){
+	tiempoActual = glutGet(GLUT_ELAPSED_TIME);
+	//printf("%d\n", tiempoActual);
+	delta = (double)(tiempoActual - tiempoAnterior)/1000;
+	tiempoAnterior = tiempoActual;
+
+}
+
+void animacionFrames(){
+	int par = (tiempoActual/300)%2;
+	if(lim_sup> 2*(a)){
+		par = 1;
+
+	}
+	if (par){
+		b = b + delta*100;
+		lim_sup = lim_sup - tiempoActual/20000;
+	}else{
+		b = b - delta*100;
+		lim_sup = lim_sup + tiempoActual/20000;
+	}
+	if(lim_sup >= 2*a){
+		printf("%d\n", tiempoActual);
+	}
+
 }
 
 void display() {
-	glTranslatef(x_pos, y_pos, z_pos);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	 glPushMatrix();
 	glColor3f(0.8,1.0,0.4);
+	glTranslatef(x_pos, y_pos, z_pos);
+	GLfloat c = sqrt((x_i - a)*(x_i - a) + b*b); //Directriz de la parabola
+	//int lim_sup = x_i + (x_i/2) + tiempoActual/80;
+	GLfloat y;
+	float incremento = (lim_sup-x_i)/32;
+	glBegin(GL_LINE_STRIP);
+	for(GLfloat x = x_i; x <= lim_sup; x+=incremento) {
+		y = ((x - a)*(x - a) + b*b - c*c)/(2*(b-c));
+		glVertex3f(x, y, z_pos);
+
+	}
+	glEnd();
+	/**
+	glColor3f(0.8,1.0,0.4);
+	glTranslatef(x_pos, y_pos, z_pos);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	GLfloat a = x_i*1.5; // Coordenada x de foco
+	GLfloat b = 300; //Coordenada y del foco
+	GLfloat c = sqrt((x_i - a)*(x_i - a) + b*b); //Directriz de la parabola
+	int lim_sup = x_i + (x_i/2);
+	GLfloat y;
+	glBegin(GL_POINTS);
+	for(GLfloat x = x_i; x <= lim_sup; x+=0.05) {
+		y = ((x - a)*(x - a) + b*b - c*c)/(2*(b-c));
+		glVertex3f(x, y, z_pos);
+	}
+	glEnd();
+	b = b - 100;
+	lim_sup = lim_sup + 10;
+	c = sqrt((x_i - a)*(x_i - a) + b*b);
+	glBegin(GL_POINTS);
+		for(GLfloat x = x_i; x <= lim_sup; x+=0.05) {
+			y = ((x - a)*(x - a) + b*b - c*c)/(2*(b-c));
+			glVertex3f(x, y, z_pos);
+		}
+		glEnd();*/
+
+	/**
 	GLfloat verticeY = 500;
 	GLfloat verticeX = 100;
 	GLfloat y = 0;
@@ -30,15 +107,60 @@ void display() {
 	int lim_sup = h + p;
 	int altura_pasto = 5;
 	int inicio = h- 15*p;
-
+	glTranslatef(x_pos, y_pos, z_pos);
 	glClear(GL_COLOR_BUFFER_BIT);
 	glBegin(GL_POINTS);
 	for(GLfloat x = inicio; x <= lim_sup; x+=0.05) {
-		y = -((x - h)*(x - h))/(4*p) + altura_pasto;
-		glVertex2f(verticeX + x, verticeY + y);
+		y = -((x - h)*(x - h) + (h*h))/(4*p);
+		glVertex3f(verticeX + x, verticeY + y, z_pos);
 	}
-	glEnd();
-	glFlush();
+	glEnd();*/
+
+	//glFlush();
+	glPopMatrix();
+	glutSwapBuffers();
+}
+
+void animar(){
+	calculaTiempo();
+	animacionFrames();
+	glutPostRedisplay();
+}
+
+/**
+ * Funcion que define el teclado
+ */
+void keyboard (unsigned char key, int x, int y) {
+	switch (key) {
+	case 'x':
+		x_pos -= 0.5;
+		glutPostRedisplay();
+		break;
+	case 'X':
+		x_pos += 0.5;
+		glutPostRedisplay();
+		break;
+	case 'y':
+		y_pos -= 0.5;
+		glutPostRedisplay();
+		break;
+	case 'Y':
+		y_pos += 0.5;
+		glutPostRedisplay();
+		break;
+	case 'z':
+		z_pos -= 0.5;
+		glutPostRedisplay();
+		break;
+	case 'Z':
+		z_pos += 0.5;
+		glutPostRedisplay();
+		break;
+	case 'q':
+		exit(0);
+		break;
+	default: break;
+	}
 }
 
 int main(int argc, char** argv) {
@@ -48,8 +170,12 @@ int main(int argc, char** argv) {
 	glutInitDisplayMode(GLUT_RGB|GLUT_SINGLE);
 	glutCreateWindow("Quadratic Formula");
 	init();
+	glutIdleFunc(animar);
+	glutKeyboardFunc(keyboard);
 	glutDisplayFunc(display);
-	glutMainLoop();
 
+	tiempoActual = tiempoAnterior = 0;
+	glutMainLoop();
 	return 0;
+
 }
